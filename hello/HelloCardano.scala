8897075aeee7695f11bcd863571c176c2fa49b9e
fernweh0 <aleksey.khodakovskiy@gmail.com>
2025-11-20 18:04:32 +0100

    feat: updating to the recent versions

--- hello/HelloCardano.scala
+++ hello/HelloCardano.scala
@@ -4,8 +4,6 @@ import scalus.*
 import scalus.builtin.Data
 import scalus.ledger.api.v3.{PubKeyHash, TxInfo, TxOutRef}
 import scalus.prelude.*
-import scalus.prelude.Option.Some
-import scalus.prelude.Prelude.*
 
 /** This validator demonstrates two key validation checks:
     * 1. It verifies that the transaction is signed by the owner's public key hash (stored in the datum)
@@ -16,15 +14,15 @@ import scalus.prelude.Prelude.*
 
 @Compile
 object HelloCardano extends Validator:
-    override def spend(
+    inline override def spend(
         datum: Option[Data],
         redeemer: Data,
         tx: TxInfo,
         outRef: TxOutRef
     ): Unit =
-        val Some(ownerData) = datum: @unchecked
-        val owner = ownerData.to[PubKeyHash]
+        val owner = datum.getOrFail("Datum not found").to[PubKeyHash]
         val signed = tx.signatories.contains(owner)
         require(signed, "Must be signed")
-        val saysHello = redeemer.to[String] == "Hello, Cardano!"
+        val saysHello = redeemer.to[String] == "Hello, World!"
         require(saysHello, "Invalid redeemer")
+
--- hello/HelloCardano.test.scala
+++ hello/HelloCardano.test.scala
@@ -1,27 +1,22 @@
 package validator
 
-import munit.FunSuite
+import org.scalatest.funsuite.AnyFunSuite
 import scalus.*
 import scalus.Compiler.compile
 import scalus.builtin.ByteString.*
+import scalus.builtin.Data
 import scalus.builtin.Data.toData
-import scalus.builtin.{ByteString, Data}
+import scalus.cardano.ledger.ExUnits
 import scalus.ledger.api.v1.PubKeyHash
 import scalus.prelude.*
 import scalus.testkit.ScalusTest
-import scalus.uplc.*
-import scalus.uplc.eval.*
 
 import scala.language.implicitConversions
-import scala.math.Ordering.Implicits.*
-
-class HelloCardanoSpec extends FunSuite, ScalusTest {
+class HelloCardanoTest extends AnyFunSuite with ScalusTest {
 
     test("Hello Cardano") {
-        val ownerPubKey = PubKeyHash(
-          hex"1234567890abcdef1234567890abcdef1234567890abcdef12345678"
-        )
-        val message = "Hello, Cardano!".toData
+        val ownerPubKey = PubKeyHash(hex"1234567890abcdef1234567890abcdef1234567890abcdef12345678")
+        val message = "Hello, World!".toData
         val context = makeSpendingScriptContext(
           datum = ownerPubKey.toData,
           redeemer = message,
@@ -29,8 +24,6 @@ class HelloCardanoSpec extends FunSuite, ScalusTest {
         )
 
         val result = compile(HelloCardano.validate).runScript(context)
-
         assert(result.isSuccess)
-        assert(result.budget <= ExBudget(ExCPU(62000000), ExMemory(240000)))
     }
 }
